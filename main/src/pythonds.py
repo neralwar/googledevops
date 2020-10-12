e60044cb992615f77ad7b6f57f8f1456ff87a942
Ashish <ashish.neralwar@googlemail.com>
2020-10-12 13:15:09 +0530

    Latest Update

---
+++ main/src/pythonds.py
@@ -0,0 +1,13 @@
+# Python DS Hands-On
+# List
+
+mylist = ["Ashish","Ramesh","Neralwar"]
+
+for mylistitem in mylist:
+    print(mylistitem)
+
+lambdademo = lambda a , b : a * b
+print(lambdademo(5,4))
+
+    
+ 
